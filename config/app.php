8def42a56cd61f18b5e8a80f101e69c43b0c3592
root <root@ip-192-168-12-43>
2022-02-08 22:20:55 -0500

    Mc

--- config/app.php
+++ config/app.php
@@ -13,7 +13,8 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'PRM'),
+    'name_url' => env('APP_NAME_URL', 'PRM'),
 
     /*
     |--------------------------------------------------------------------------
